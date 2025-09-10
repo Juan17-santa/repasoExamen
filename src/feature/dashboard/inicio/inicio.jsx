79c45c65836a878fbc81f5f4f1b78ce68a3bd67a
Juan17-santa <juanmasanta2007gmail.com>
2025-09-10 16:08:27 -0500

    Commit despues de solucionar el error

--- src/feature/dashboard/inicio/inicio.jsx
+++ src/feature/dashboard/inicio/inicio.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
-export default function Home() {
+export default function Inicio() {
     const navigate = useNavigate()
     const [empresas, setEmpresas] = useState([])
     const [empleados, setEmpleados] = useState([])
--- src/routes/routesApp.jsx
+++ src/routes/routesApp.jsx
@@ -3,9 +3,9 @@ import Landing from "../feature/landing/landing";
 import Login from "../feature/landing/pages/auth/login";
 import Registrate from "../feature/landing/pages/auth/register";
 import Dashboard from "../feature/dashboard/dashboard";
-import Home from "../feature/dashboard/home.jsx";
 import Empresas from "../feature/dashboard/empresas/empresas";
 import Empleados from "../feature/dashboard/empleados/empleados";
+import Inicio from "../feature/dashboard/inicio/inicio";
 
 export default function RoutesApp() {
     return (
@@ -17,7 +17,7 @@ export default function RoutesApp() {
                 </Route>
 
                 <Route path="/dashboard" element={<Dashboard />}>
-                    <Route index element={<Home />} />
+                    <Route index element={<Inicio />} />
                     <Route path="empresas" element={<Empresas />} />
                     <Route path="empleados" element={<Empleados />} />
                 </Route>
